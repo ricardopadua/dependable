5bd11db221425aa65d6e218443a5146f3d81a5fa
ricardopadua <ricardo.padua4@gmail.com>
2025-07-09 09:51:53 -0300

    first commit

---
+++ lib/dependable.ex
@@ -0,0 +1,18 @@
+defmodule Dependable do
+  @moduledoc """
+  Documentation for `Dependable`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dependable.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
